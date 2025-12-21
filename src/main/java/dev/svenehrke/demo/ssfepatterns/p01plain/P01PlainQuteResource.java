172f4527a68bb76593882f723f0453b1c1b6b1f9
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-21 10:10:57 +0100

    plain qute pages: first simple application page

---
+++ src/main/java/dev/svenehrke/demo/ssfepatterns/p01plain/P01PlainQuteResource.java
@@ -0,0 +1,20 @@
+package dev.svenehrke.demo.ssfepatterns.p01plain;
+
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/p01plain")
+public class P01PlainQuteResource {
+
+	@CheckedTemplate(basePath = "dev/svenehrke/demo/ssfepatterns/p01plain")
+	static class Templates {
+		public static native TemplateInstance page01();
+	}
+	@GET
+	@Path("page01")
+	public TemplateInstance page01() {
+		return Templates.page01();
+	}
+}
