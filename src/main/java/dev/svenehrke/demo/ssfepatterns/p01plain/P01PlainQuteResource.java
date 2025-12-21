621644b76ee8e856715a67adb3cc16159cf32152
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-21 15:10:49 +0100

    plain qute pages: second example with component

--- src/main/java/dev/svenehrke/demo/ssfepatterns/p01plain/P01PlainQuteResource.java
+++ src/main/java/dev/svenehrke/demo/ssfepatterns/p01plain/P01PlainQuteResource.java
@@ -11,10 +11,16 @@ public class P01PlainQuteResource {
 	@CheckedTemplate(basePath = "dev/svenehrke/demo/ssfepatterns/p01plain")
 	static class Templates {
 		public static native TemplateInstance page01();
+		public static native TemplateInstance page02withcomponent();
 	}
 	@GET
 	@Path("page01")
 	public TemplateInstance page01() {
 		return Templates.page01();
+	}
+	@GET
+	@Path("page02withcomponent")
+	public TemplateInstance page02withcomponent() {
+		return Templates.page02withcomponent();
 	}
 }
