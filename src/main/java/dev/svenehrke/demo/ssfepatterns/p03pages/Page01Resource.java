e044c955703dbbcaf7f87c406ba6cb30b082efc2
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-25 08:17:56 +0100

    page patterns section: started

---
+++ src/main/java/dev/svenehrke/demo/ssfepatterns/p03pages/Page01Resource.java
@@ -0,0 +1,18 @@
+package dev.svenehrke.demo.ssfepatterns.p03pages;
+
+import dev.svenehrke.demo.ssfepatterns.p01plain.Page01Code;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/")
+public class Page01Resource {
+
+	public static final String URL = "/p03pages/page01";
+
+	@GET
+	@Path(URL)
+	public TemplateInstance page() {
+		return P03PagesTemplates.Templates.page01contentpage(new Page01Code());
+	}
+}
