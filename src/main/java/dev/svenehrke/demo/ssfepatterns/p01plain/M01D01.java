3387d28698f633f00ea583aaae41e5fa4d80ce51
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-26 12:43:59 +0100

    M01Page01: moved controller code from Resource to M01D01

--- src/main/java/dev/svenehrke/demo/ssfepatterns/p01plain/M01D01.java
+++ src/main/java/dev/svenehrke/demo/ssfepatterns/p01plain/M01D01.java
@@ -1,10 +1,15 @@
 package dev.svenehrke.demo.ssfepatterns.p01plain;
 
 import dev.svenehrke.demo.ssfepatterns.main.MainCardInfo;
+import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.RawString;
 import io.quarkus.qute.TemplateData;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 
 @TemplateData(namespace = "m01d01")
+@Path("/")
 public class M01D01 {
 	public static final String URL = "/p01plain/page01";
 	public static final MainCardInfo CARD_INFO  = new MainCardInfo(
@@ -14,5 +19,14 @@ public class M01D01 {
 		null,
 		new RawString("Demo...")
 	);
+	@CheckedTemplate(basePath = "dev/svenehrke/demo/ssfepatterns/m01plain")
+	static class Templates {
+		public static native TemplateInstance page01(Page01Code code);
+	}
+	@GET
+	@Path(M01D01.URL)
+	public TemplateInstance page() {
+		return M01D01.Templates.page01(new Page01Code());
+	}
 
 }
--- src/main/java/dev/svenehrke/demo/ssfepatterns/p01plain/M01PlainTemplates.java
+++ src/main/java/dev/svenehrke/demo/ssfepatterns/p01plain/M01PlainTemplates.java
@@ -6,7 +6,6 @@ import io.quarkus.qute.TemplateInstance;
 public class M01PlainTemplates {
 	@CheckedTemplate(basePath = "dev/svenehrke/demo/ssfepatterns/m01plain")
 	static class Templates {
-		public static native TemplateInstance page01(Page01Code code);
 		public static native TemplateInstance page02withcomponent(Page02Code code);
 		public static native TemplateInstance page03withparamscomponent(Page03Code code);
 		public static native TemplateInstance page04withcontentparamscomponent(Page04Code code);
