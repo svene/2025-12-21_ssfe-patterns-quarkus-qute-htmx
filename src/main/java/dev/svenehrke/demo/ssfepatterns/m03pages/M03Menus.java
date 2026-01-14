c86d48dd7c7f0db5eb57bae5c8c2a91f13fb5977
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-01-14 07:12:10 +0100

    migrate to routing

--- src/main/java/dev/svenehrke/demo/ssfepatterns/m03pages/M03Menus.java
+++ src/main/java/dev/svenehrke/demo/ssfepatterns/m03pages/M03Menus.java
@@ -4,33 +4,31 @@ import dev.svenehrke.demo.ssfepatterns.m00main.MainCardInfo;
 import io.quarkus.qute.RawString;
 import io.quarkus.qute.TemplateData;
 
-import static dev.svenehrke.demo.ssfepatterns.m03pages.M03Routing.M03D03_URL;
-
 @TemplateData(namespace = TemplateData.SIMPLENAME)
 public interface M03Menus {
 	MainCardInfo M03D01_CARD_INFO = new MainCardInfo(
-		M03Routing.M03D01_URL,
+		M03Routing.M03D01Routing.URL,
 		new RawString("Content Page"),
 		null,
 		new RawString("Recommended for most use cases"),
 		new RawString("Demo...")
 	);
 	MainCardInfo M03D02_CARD_INFO = new MainCardInfo(
-		M03Routing.M03D02_URL + "?greeting=Hello",
+		M03Routing.M03D02Routing.URL + "?greeting=Hello",
 		new RawString("Content Page"),
 		new RawString("taking a parameter"),
 		new RawString("When the page itself needs a parameter"),
 		new RawString("Demo...")
 	);
 	MainCardInfo M03D03_CARD_INFO = new MainCardInfo(
-		M03D03_URL + "?greeting=Hello",
+		M03Routing.M03D03Routing.URL,
 		new RawString("Custom Page"),
 		new RawString("taking a parameter"),
 		new RawString("When the opinionated content page does not fit"),
 		new RawString("Demo...")
 	);
 	MainCardInfo M03D04_CARD_INFO = new MainCardInfo(
-		M03Routing.PAGE1_URL,
+		M03Routing.M03D04Routing.PAGE1_URL,
 		new RawString("MPA Example"),
 		new RawString("with menu"),
 		null,
--- src/main/java/dev/svenehrke/demo/ssfepatterns/m03pages/M03Routing.java
+++ src/main/java/dev/svenehrke/demo/ssfepatterns/m03pages/M03Routing.java
@@ -8,53 +8,62 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
 
-@Path("/")
 @TemplateData(namespace = TemplateData.SIMPLENAME)
 public class M03Routing {
-
-	// D01 --------------------------------------------
-	public static final String M03D01_URL = "/m03/d01";
-	@GET
-	@Path(M03Routing.M03D01_URL)
-	public TemplateInstance page() {
-		return Templates.m03d01();
+	@Path("/")
+	public static class M03D01Routing {
+		public static final String URL = "/m03/d01";
+		@GET
+		@Path(URL)
+		public TemplateInstance page() {
+			return Templates.m03d01();
+		}
 	}
 
-	// D02 --------------------------------------------
-	public static final String M03D02_URL = "/m03/d02";
-
-	@GET
-	@Path(M03Routing.M03D02_URL)
-	public TemplateInstance m03d02e(
-		@QueryParam("greeting") @DefaultValue("-") String greeting
-	) {
-		return Templates.m03d02(greeting);
+	@Path("/")
+	public static class M03D02Routing {
+		public static final String URL = "/m03/d02";
+		@GET
+		@Path(URL)
+		public TemplateInstance m03d02e(
+			@QueryParam("greeting") @DefaultValue("-") String greeting
+		) {
+			return Templates.m03d02(greeting);
+		}
 	}
 
-	// D03 --------------------------------------------
-	public static final String M03D03_URL = "/m03/d03";
-	@GET
-	@Path(M03D03_URL)
-	public TemplateInstance m03d03(
-		@QueryParam("greeting") @DefaultValue("-") String greeting
-	) {
-		return Templates.m03d03(greeting);
+	@Path("/")
+	public static class M03D03Routing {
+		public static final String URL = "/m03/d03";
+		@GET
+		@Path(URL)
+		public TemplateInstance m03d02e(
+			@QueryParam("greeting") @DefaultValue("-") String greeting
+		) {
+			return Templates.m03d02(greeting);
+		}
 	}
+
+	@Path("/")
+	@TemplateData(namespace = TemplateData.SIMPLENAME)
+	public static class M03D04Routing {
+		public static final String PAGE1_URL = "/m03/d04p1";
+		public static final String PAGE2_URL = "/m03/d04p2";
+		@GET
+		@Path(PAGE1_URL)
+		public TemplateInstance page1() {
+			return Templates.m03d04p1();
+		}
+
+		@GET
+		@Path(PAGE2_URL)
+		public TemplateInstance page2() {
+			return Templates.m03d04p2();
+		}
+	}
+
 
 	// D04 --------------------------------------------
-	public static final String PAGE1_URL = "/m03/d04p1";
-	public static final String PAGE2_URL = "/m03/d04p2";
-	@GET
-	@Path(PAGE1_URL)
-	public TemplateInstance page1() {
-		return Templates.m03d04p1();
-	}
-
-	@GET
-	@Path(PAGE2_URL)
-	public TemplateInstance page2() {
-		return Templates.m03d04p2();
-	}
 
 	@CheckedTemplate(basePath = "dev/svenehrke/demo/ssfepatterns/m03pages")
 	private static class Templates {
