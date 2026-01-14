a67dbca5b631f8c2c5cf6ce84481b79498e79b1a
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-01-14 06:10:51 +0100

    card infos

--- src/main/java/dev/svenehrke/demo/ssfepatterns/m03pages/M03Menus.java
+++ src/main/java/dev/svenehrke/demo/ssfepatterns/m03pages/M03Menus.java
@@ -8,7 +8,21 @@ import static dev.svenehrke.demo.ssfepatterns.m03pages.M03Routing.M03D03_URL;
 
 @TemplateData(namespace = TemplateData.SIMPLENAME)
 public interface M03Menus {
-	MainCardInfo M03_CARD_INFO = new MainCardInfo(
+	MainCardInfo M03D01_CARD_INFO = new MainCardInfo(
+		M03D01.URL,
+		new RawString("Content Page"),
+		null,
+		new RawString("Recommended for most use cases"),
+		new RawString("Demo...")
+	);
+	MainCardInfo M03D02_CARD_INFO = new MainCardInfo(
+		M03D02.URL + "?greeting=Hello",
+		new RawString("Content Page"),
+		new RawString("taking a parameter"),
+		new RawString("When the page itself needs a parameter"),
+		new RawString("Demo...")
+	);
+	MainCardInfo M03D03_CARD_INFO = new MainCardInfo(
 		M03D03_URL + "?greeting=Hello",
 		new RawString("Custom Page"),
 		new RawString("taking a parameter"),
